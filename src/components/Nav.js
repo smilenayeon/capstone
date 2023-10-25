a3bdf63ac1e371bff633c00ba93b99da5610e2ce
Na Yeon <smilenayeon@naver.com>
2023-10-25 21:57:01 +0900

    started to work on navbar. logo is in. meta tags are set.

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+    <div className="navBar">
+        <img className="navLogo" src="images/logo1.png" alt="logo"></img>
+        
+        </div>
+    </div>
+  )
+}
+
+export default Nav;
